fa77c19a119c6f326f6a141bbae54ec411bc82a9
LikithMeruvu <codebook369@gmail.com>
2024-02-24 22:49:45 +0530

    v2 with gemini

--- jarvis_ai.py
+++ jarvis_ai.py
@@ -1,25 +1,33 @@
-# import time
+
 import pyttsx3
 import speech_recognition as sr
 import datetime
 import wikipedia
-import pywhatkit as pwt
 import webbrowser as wb
 import os
 import random
-import datetime
-from bardapi import Bard
+import pywhatkit as pwt
+import google.generativeai as genai # Import GeminiPro from gemini_pro module
+from dotenv import load_dotenv  # Import load_dotenv from dotenv module
 
+# Load API key from .env file
+load_dotenv()
+API_KEY = os.getenv('GEMINI_PRO_API_KEY')
 
+# Initialize pyttsx3 engine
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)
 engine.setProperty('rate', 170)
 
+# Function to speak out the data
+
 
 def speak(data):
     engine.say(data)
     engine.runAndWait()
+
+# Function to take user command via microphone input
 
 
 def takeCommand():
@@ -33,7 +41,7 @@ def takeCommand():
     try:
         print("Recognizing...")
         # Using google for voice recognition.
-        query = r.recognize_google(audio, language='en-us ')
+        query = r.recognize_google(audio,language='en-us')
         print(f"You said: {query}")  # User query will be printed.
 
     except Exception as e:
@@ -43,6 +51,9 @@ def takeCommand():
         # speak("Say that again please")   #Say that again will be printed in case of improper voice
         return "None"  # None string will be returned
     return query
+
+
+# Function to greet the user based on the time of day
 
 
 def wish_me(greet="jarvis Here, how can i help you"):
@@ -55,6 +66,8 @@ def wish_me(greet="jarvis Here, how can i help you"):
         speak("hi, good evening")
     speak(greet)
 
+# Function to get information from Wikipedia based on user query
+
 
 def get_info_from_wikipedia(query: str, lines: int):
     speak("searching wikipedia")
@@ -62,6 +75,8 @@ def get_info_from_wikipedia(query: str, lines: int):
     results = wikipedia.summary(query, sentences=lines)
     print(results)
     speak(results)
+
+# Function to open Google search based on user query
 
 
 def Open_Google(query: str):
@@ -73,16 +88,19 @@ def Open_Google(query: str):
     print(query)
     pwt.search(query)
 
+# Function to open YouTube and play a specific song based on user query
+
 
 def Open_YouTube(query: str):
     speak("opening youtube")
-    # query = query.replace("open youtube", "")
     li = query.split(" ")
     i = li.index("youtube")
     res = li[i+3:len(li)]
     query = ' '.join([str(elem) for elem in res])
     print(query)
     pwt.playonyt(query)
+
+# Function to play random music from a specified directory
 
 
 def Play_music():
@@ -91,35 +109,47 @@ def Play_music():
     os.startfile(os.path.join(
         songs_path, songs[random.randrange(0, len(songs))]))
 
+# Function to open Visual Studio Code application
+
 
 def Open_Code():
     codepath = "C:\\Users\\91630\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Visual Studio Code\\Visual Studio Code.lnk"
     speak("opening v s code")
     os.startfile(codepath)
 
-def Ask_Bard(Query):
-   token = 'ZAiKGeBg0eL15j1vGK6jFzU4V46aODCCa-3LyqwSLpGHbPYDqGtDuVkFlkZ1knOnpPYZEA.'
-   bard = Bard(token=token)
-   response = bard.get_answer(Query)['content']
-   print(response)
-   speak(response)
+# Function to interact with Gemini Pro API for answering queries
 
 
+genai.configure(api_key = API_KEY)
+def Ask_GeminiPro(Query):
+    speak("Searching Internet wait for a second")
+    model = genai.GenerativeModel('gemini-pro')
+    response = model.generate_content(Query)
+    print(response.text)
+    speak(response.text)
+
+# Function to send WhatsApp messages to specified contacts
 
 
 def WhatsApp():
     speak("For Whom should i send message")
+
     Contacts = {
-        'mom': 789XXXXXXX,
-        'friend': 756XXXXXXX,
-        'me': 810XXXXXXX,
-        'jay': 996XXXXXXX,
-        'tory' : 92XXXXXXX 
+        'mom': 789345734857,
+        'friend': 756345734857,
+        'me': 810345734857,
+        'jay': 996345734857,
+        'tory': 92345734857
     }
+
     to = takeCommand()
+
     speak("what message should i send")
+
     msg = takeCommand()
+
     speak(f"sending message to {to}")
+
     try:
         # change your phone country code mine is +91
         pwt.sendwhatmsg_instantly(f"+91{Contacts[to]}", msg)
@@ -130,13 +160,13 @@ def WhatsApp():
 
 if __name__ == '__main__':
     wish_me()
+
     while True:
         query = takeCommand().lower()
 
         if "wikipedia" in query:
             get_info_from_wikipedia(query, 5)
 
-        # say :- open youtube and play {Your desired song}
         elif 'open youtube' in query:
             Open_YouTube(query)
 
@@ -151,7 +181,7 @@ if __name__ == '__main__':
             Play_music()
 
         elif 'open code' in query:
-            Open_Code(query)
+            Open_Code()
 
         elif 'whatsapp message' in query:
             WhatsApp()
@@ -160,5 +190,5 @@ if __name__ == '__main__':
             speak("have a good day mate, see you later")
             exit()
 
-        else:
-            Ask_Bard(query)
+        elif 'jarvis' in query:
+            Ask_GeminiPro(query)
